d72d9a1b71ec68ee8ad8f2d498ad42715ad1e26e
Aurelienpt <aurelpt@gmail.com>
2019-03-17 13:19:35 +0000

    new changes

---
+++ main.js
@@ -0,0 +1,8 @@
+// const lien = "https://cors-anywhere.herokuapp.com/https://euw.op.gg/summoner/userName=Trrans"
+
+// fetch(lien).then(response => {
+//     response.text().then(data => {
+//         const rank = document.getElementsByClassName("TierRank");
+        
+//     })
+// })
