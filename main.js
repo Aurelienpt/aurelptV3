45e1fcd1867411bd708cd0f48fa46eb9f6161748
Aurelienpt <aurelpt@gmail.com>
2019-03-17 15:17:37 +0000

    new changes

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
-// const lien = "https://cors-anywhere.herokuapp.com/https://euw.op.gg/summoner/userName=Trrans"
+const lien = "https://euw1.api.riotgames.com/lol/league/v4/positions/by-summoner/gKOWWzb3nvAP73XnsOaNPz9lbQ6e3c32vCWYx5YMPnSNeYI?api_key=RGAPI-c60e130e-811e-4d3b-9748-aca9e17df4f0;"
 
-// fetch(lien).then(response => {
-//     response.text().then(data => {
-//         const rank = document.getElementsByClassName("TierRank");
-        
-//     })
-// })
+fetch(lien).then(data => {
+    print(data)
+})
